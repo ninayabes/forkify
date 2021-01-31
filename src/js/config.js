d90839590ad900d323d305d205a3b5cc69a3fbbf
ninayabes <ninac.yabes@gmail.com>
2021-01-31 09:00:12 -0800

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b1372146-242a-437e-92e2-7a1d50839408';
+export const MODAL_CLOSE_SEC = 2.5;
